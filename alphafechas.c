58c71030e83c9d1aa6b0cca9054167d1dc06e6e0
Lucas <lucas.cezar@alumnos.upm.es>
2021-05-07 00:58:53 +0200

    Remate
    Funcion calculo de dias totales de viaje. Seleccion de numero de personas y tarifas y su implementacion en el menu principal.

--- alphafechas.c
+++ alphafechas.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
-int d0, m0, a0, d1, m1, a1;
+
+
+int d0, m0, a0, d1, m1, a1, time, np, clase, tar;
+// d, m, a son: dia, mes y año de inicio y fin consecutivamente. time es la variable tiempo de transcurso del viaje. np el numero de personas. clase es el tipo de turismo escojido. tar la tarifa.
+char tipo[30];
+char descuento[30];
 
 typedef struct {int d, m, a; }fecha;
 
@@ -11,10 +17,30 @@ typedef struct
      int np;
     }viaje;
 
+int dias(){
+    if (m1 == m0){
+     time = d1 - d0;
+     //printf("dias: %d\n", time);
+    }
+    if (m1 > m0){
+     time = d1 + (31 * (m1 - m0) - d0 + 1);
+     //printf("dias: %d\n", time);
+    }
+}
+
+float precio(){
+//    total = np *
+}
+
 void showData(){
-    printf("    DATOS DEL VIAJE    \n");
-    printf(" .- Fecha de entrda: %d/%d/%d\n", d0, m0, a0);
-    printf(" .- Fecha de salida: %d/%d/%d\n\n\n", d1, m1, a1);
+    printf("-------- DATOS DEL VIAJE --------\n");
+    printf(" - Fecha de entrda: %d/%d/%d\n", d0, m0, a0);
+    printf(" - Fecha de salida: %d/%d/%d\n", d1, m1, a1);
+    printf("     *La duraci%cn de su viaje ser%c de %d d%cas\n", 162, 160, time, 161);
+    printf(" - N%cmero de personas: %d\n", 163, np);
+    printf(" - Clase de viaje: %s\n", tipo);
+    printf(" - Destino seleccionado: \n");
+    printf(" - Tarifa: %s\n\n\n", descuento);
 }
 
 
@@ -27,10 +53,10 @@ int main()
 
     do{
    system("cls");
-    printf("VIAJA CON DELLPING, EXITO ASEGURADO \n \n \n");
+    printf("VIAJA CON DELLPING, EXITO ASEGURADO \n \n");
     showData();
-    printf("Seleccione la opcion deseada para la eleccion de su viaje: \n ");
-    printf("a.Fechas deseadas para viajar\n b.Cantidad de personas que van a viajar\n c.Preferencias de viaje\n d.Destinos disponibles\n e.Ver tarifas disponibles\n f.Salir del programa\n");
+    printf("Seleccione la opci%cn deseada para la elecci%cn de su viaje: \n", 162, 162);
+    printf(" a.Fechas deseadas para viajar\n b.Cantidad de personas que van a viajar\n c.Preferencias de viaje\n d.Destinos disponibles\n e.Ver tarifas disponibles\n f.Salir del programa\n\n");
     scanf("%c", &op);
 
 
@@ -83,31 +109,52 @@ int main()
                         v.salida.d = d1;
                         v.salida.m = m1;
                         v.salida.a = a1;
-
-
+        dias();
         printf("Pulse un numero si desea volver al menu principal. \n");
         scanf("%d", &num);
-
-
         break;
+
 
         case 'b':
         case 'B':
         printf("HA SELECCIONADO LA OPCION PERSONAS QUE VAN A VIAJAR. \n");
 
+        printf("Introduzca el numero de personas que van a participar en el viaje: ");
+            scanf("%d", &np);
+
         printf("Pulse un numero si desea volver al menu principal. \n");
         scanf("%d", &num);
         break;
+
 
         case 'c':
         case 'C':
+
         printf("HA SELECCIONADO LA OPCION PREFERENCIAS DE VIAJE. \n");
+
+
+
+            bool error1 = true;
+            while(error1){
+
+                printf("Seleccione la clase de viaje que desea realizar marcando el numero correspondiente:\n");
+                printf(" 1.- Turismo cultural\n 2.- Turismo rural\n 3.- Turismo de sol y playa\n");
+                scanf("%d", &clase);
+                //printf("Ha seleccionado %d\n", clase);
+
+                if (clase == 1){strcpy(tipo, "Turismo cultural"); error1 = false;}
+                else if (clase == 2){strcpy(tipo, "Turismo rural"); error1 = false;}
+                else if (clase == 3){strcpy(tipo, "Turismo de sol y playa"); error1 = false;}
+                else {error1 = true; printf("Por favor seleccione una de las opciones posibles\n");}
+            }
+            printf("Ha seleccionado %s\n", tipo);
+
 
         printf("Pulse un numero si desea volver al menu principal. \n");
         scanf("%d", &num);
 
-
         break;
+
 
         case 'd':
         case 'D':
@@ -120,7 +167,17 @@ int main()
 
         case 'e':
         case 'E':
-        printf("HA SELECCIONADO LA OPCION DE VER TARIFAS DISPONIBLES. \n");
+        printf("HA SELECCIONADO LA OPCION TARIFAS DISPONIBLES. \n");
+        printf("Marque la tarifa correspondiente:\n 1.- Familia numerosa general: 30%c\n 2.- Familia numerosa especial: 50%c\n 3.- Mayores de 65 a%cos: 25%c\n", 37, 37, 164, 37);
+
+            bool error2 = true;
+            while(error2){
+            scanf("%d", &tar);
+                if (tar == 1) {(error2 = false); printf("Ha seleccionado Familia numerosa general\n"); strcpy(descuento, "Familia numerosa general");}
+                else if (tar == 2) {(error2 = false); printf("Ha seleccionado Familia numerosa especial\n"); strcpy(descuento, "Familia numerosa especial");}
+                else if (tar == 3) {(error2 = false); printf("Ha seleccionado Mayores de 65 a%cos\n", 164); strcpy(descuento, "Mayores de 65");}
+                else {(error2 = true); printf("Por favor, seleccione una opci%cn v%clida\n", 162, 160);}
+            }
 
         printf("Pulse un numero si desea volver al menu principal. \n");
         scanf("%d", &num);
@@ -130,6 +187,7 @@ int main()
 
         case 'f':
         case 'F':
+        system("cls");
         printf("SALIR. \n");
 
         break;
